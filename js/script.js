fc55ce950cc2f41826d0def7551ad47aea8cac54
omteja04 <22P31A0539@acet.ac.in>
2024-05-25 21:10:57 +0530

    Script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// import images as relative image path won't work with vite/vercel.
+import check from "../assets/check.svg";
+import star from "../assets/star.svg";
+import sushi12 from "../assets/sushi-12.png";
+import sushi11 from "../assets/sushi-11.png";
+import sushi10 from "../assets/sushi-10.png";
+import sushi8 from "../assets/sushi-8.png";
+import sushi7 from "../assets/sushi-7.png";
+import sushi6 from "../assets/sushi-6.png";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 
